e6bf91c822e1b9f19b438baaed55f1898864e5a2
Umudisly <112596403+Umudisly@users.noreply.github.com>
2025-02-23 21:16:01 -0500

    update

--- demo.py
+++ demo.py
@@ -1,3 +1,1 @@
-Hello evryone
-Enjoy Jemkins on Sunday
-This message is from python code used in jenkins __build_class__
+Hello evryone, Enjoy Jemkins on Sunday, This message is from python code used in jenkins __build_class__
